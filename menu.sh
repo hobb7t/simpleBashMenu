4e5a5fd005b313945fd14612f476411544c79358
hobb7t <44687520+hobb7t@users.noreply.github.com>
2024-01-13 00:56:40 +0200

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -1,45 +1,20 @@
-#!/bin/bash
+!/bin/bash
 
-display_menu() {
-  echo "Menu:"
-  echo "1. Your name"
-  echo "2. Random number"
-  echo "3. Exit"
-}
+echo "Select one option from the  menu please: "
 
-display_name() {
-  read -p "Enter your full name: " name
-  echo "Using cowsay:"
-  cowsay "$name"
-  echo "Using figlet:"
-  figlet "$name"
-}
+menu=("Your name" "Random number" "Quit")
 
-display_random_number() {
-  echo "Random number between 1 and 1000: $((RANDOM % 1000 + 1))"
-}
-
-while true; do
-  display_menu
-
-  read -p "Enter your choice (1/2/3): " choice
-
-  case $choice in
-    1)
-      display_name
-      ;;
-    2)
-      display_random_number
-      ;;
-    3)
-      echo "Exiting the script. Goodbye!"
-      exit 0
-      ;;
-    *)
-      echo "Invalid choice. Please enter 1, 2, or 3."
-      ;;
-  esac
-
-  read -p "Press Enter to continue..."
-  clear  # Clear the screen for the next iteration
+select opt in "${menu[@]}"
+do
+    case $opt in
+        "Your name")
+            echo "Anastasios Kosmidis" | cowsay;;
+        "Random number")
+           echo "echo $((1 + $RANDOM % 1000))";;
+        "Quit")
+           echo "Good bye"
+           break;;
+        *)
+           echo "Ooops, wrong option $REPLY";;
+    esac
 done
